d671645d017561e83924f2be500d90ba8afb0474
Mark <melichar20@gmail.com>
2020-09-01 09:22:32 -0400

    first commit

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
+import {Map,tileLayer,marker} from 'leaflet';
+
 
 @Component({
   selector: 'app-tab2',
@@ -9,14 +11,34 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
 export class Tab2Page {
   latitude: any = 0;
   longitude: any = 0;
+  map: Map;
+  zoomLevel: number = 11;
+  newMarker: any;
+  @ViewChild('mapId') mapContainer;
 
   constructor(private geolocation: Geolocation) {}
+
+    
+
+    displayMap() {
+      if(this.map){
+        // this.map.off();
+        // this.map.remove();
+        //document.getElementById(this.mapContainer).outerHTML = "";
+        this.loadMap();
+      }
+      this.loadMap();
+    }
+
+    // ionViewDidEnter() {
+    //   this.loadMap();
+    //   }
 
     options = {
       timeout: 10000,
       enableHighAccuracy: true,
       maximumAge: 3600
-    };
+    };    
 
     // Use geolocation to get user's device coordinates
     getCurrentCoordinates() {
@@ -26,5 +48,19 @@ export class Tab2Page {
        }).catch((error) => {
          console.log('Error getting location', error);
        });
+       console.log(`Position:` + this.latitude + this.longitude);
   }
+    loadMap() {
+      this.map = new Map('mapId').setView([this.latitude, this.longitude], this.zoomLevel);
+      tileLayer('https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', 
+      {	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      })
+      .addTo(this.map);
+      console.log(`loadMap position:` + this.latitude + this.longitude);
+    }
+
+    loadMarker() {
+      this.newMarker.setLatLng([this.latitude, this.longitude])
+      .addTo(this.map);
+    }
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import * as p5 from 'p5';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
 
-  constructor() {}
+
+export class Tab3Page implements OnInit {
+
+
+  constructor(private el: ElementRef) { }  
+
+  ngOnInit() {
+
+    const sketch = (p: any) => {
+      let snowflakes = [];
+
+      p.setup = () => {
+        const c = document.querySelector('#canvasContainer');
+        p.createCanvas(p.windowWidth , p.windowHeight ).parent(c);
+        p.fill(240);
+        p.noStroke();
+      }
+
+      p.draw = () => {
+        let t: number = p.frameCount / 60;
+        p.background('#62c3e3');
+
+        // create snowflakes each frame
+        for (let i = 0; i < p.random(5); i++) {
+          snowflakes.push(new snowflake());     
+        }
+        // update each snowflake
+        for(let flake of snowflakes) {
+          flake.update(t);
+          flake.display();
+        }
+      }
+
+      function snowflake() {
+        // initialize
+        let posX = 0;
+        let posY = p.random(-50, 0);
+        let initialangle = p.random(0, 2 * p.PI);
+        let size = p.random(2, 5);
+        // radius of snowflake spiral
+        let radius = p.sqrt(p.random(p.pow(p.windowWidth / 2, 2)));
+      
+        this.update = (time: number) => {
+          let w = 0.6;
+          let angel = w * time + initialangle;
+          posX = p.windowWidth / 2 + radius * p.sin(angel);  
+          
+          // different size snowflakes fall at slightly different y speeds
+          posY += p.pow(size, 0.5);
+          // delete snowflake if past end of screen
+          if (posY > p.windowHeight) {
+            let index = snowflakes.indexOf(this);
+            snowflakes.splice(index, 1);
+          }
+        }
+      
+        this.display = function() {
+          p.ellipse(posX, posY, size);
+        };
+      } // Snowflake
+
+    } // Sketch
+
+    const myp5 = new p5(sketch, this.el.nativeElement);
+  } // OnInit
 
 }
+
+
